2716547490c3049b6f2a8df6c83a1d2c588f2e72
BaranEgeSenol <baranegesenol@gmail.com>
2025-10-14 00:22:30 +0300

    feat: new index.html + starter React main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
-// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => (
+  <div style={{ textAlign: "center", marginTop: "20%" }}>
+    <h1>🎂 Happy Birthday! 🎉</h1>
+    <p>Made with ❤️ by Baran Ege Şenol</p>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
