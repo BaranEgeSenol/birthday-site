8ad3e5ab4f87a1086ad65f05847576f072648957
BaranEgeSenol <baranegesenol@gmail.com>
2025-10-14 01:15:19 +0300

    feat: confetti when letter opens

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,7 @@ export default function App() {
 
       {/* Mektup */}
       <LetterModal open={open} onClose={() => setOpen(false)}>
-        <p>Sevgili Ahsen,</p>
+        <p>Merhaba Ahsencim,</p>
         <p>
           Bugün senin günün. Hayatıma kattığın tüm mutluluklar için teşekkür ederim.
           Birlikte geçirdiğimiz her an benim için çok değerli. İyi ki varsın, iyi ki doğdun!
